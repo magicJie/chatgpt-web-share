6163ebc9b074ae08ca2999aefe3cc8e68737f52f
opsu <opsu@bestsrm.com>
2023-12-26 06:31:03 +0000

    refactor: 调整build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+set -e
+
 image=swr.lan.aiminjie.com/amj/chatgpt-web-share
 tag=1.0
-docker build -t $image:$tag . --build-arg https_proxy=http://192.168.231.1:7898
-if [ $? -ne 0 ]; then
-    exit $?
-fi
+docker build -t $image:$tag . --build-arg https_proxy=http://10.0.1.116:20172
 docker push $image:$tag
 
